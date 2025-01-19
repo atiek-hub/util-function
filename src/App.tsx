af8c1aac7b304eb08c702f6e09997918588e0487
km <91419145+atiek-hub@users.noreply.github.com>
2025-01-19 18:49:35 +0900

    add:stopwatch-function (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
+
+import { StopWatchFunction } from "./components/StopWatch/StopWatchFunction";
+import { TimerFunction } from "./components/Timer/TimerFunction";
 import { CalendarPage } from "./components/Calendar/CalendarPage";
-// import { TimerFunction } from "./components/Timer/TimerFunction";
 
 function App() {
   return (
     <>
-      {/* <TimerFunction /> */}
+      <TimerFunction />
+      <StopWatchFunction/>
       <CalendarPage/>
     </>
   );
